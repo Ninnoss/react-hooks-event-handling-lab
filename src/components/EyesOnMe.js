0f93c1fe21c2196ea17fc64490dc81cd0b3c7316
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-04-19 21:02:46 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+  function handleFocus() {
+    console.log('Good!');
+  }
+
+  function hanldeBlur() {
+    console.log('Hey! Eyes on me!');
+  }
+  return (
+    <button onFocus={handleFocus} onBlur={hanldeBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,4 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
-
+const Keypad = () => <input type="password" onChange={(e) => console.log("Entering password...")} />;
 export default Keypad;
